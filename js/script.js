bef301a2fa793631826725bc90ed7db87b22b505
Tomasz Rosik <Bosik27@gmail.com>
2020-04-06 22:55:35 +0200

    Add random output for wariables

--- js/script.js
+++ js/script.js
@@ -1,3 +1,37 @@
-let playerMove = `papier`;
+let randomNumber = Math.floor(Math.random() * 3 + 1);
 
-printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to' + playerMove + ', to wygrywasz!');
+console.log('Wylosowana liczba to: ' + randomNumber);
+
+let computerMove = 'nieznany ruch';
+
+if(randomNumber == 1){
+  computerMove = 'kamień';
+}
+if(randomNumber == 2){
+  computerMove = 'papier';
+}
+if(randomNumber == 3){
+  computerMove = 'nożyce';
+}
+
+printMessage('Mój ruch to: ' + computerMove);
+
+let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+
+console.log('Gracz wpisał: ' + playerInput);
+
+let playerMove = 'nieznany ruch';
+
+if(playerInput == '1'){
+  playerMove = 'kamień';
+}
+
+printMessage('Twój ruch to: ' + playerMove);
+
+if(1 > 2){
+	printMessage('Niesamowite! Jeden jest większe niż dwa!!!');
+} else if (1 == 2) {
+	printMessage('Dziwne – jeden jest równe dwa?!');
+} else {
+	printMessage('Wszystko po staremu, jeden jest mniejsze niż dwa.');
+}
