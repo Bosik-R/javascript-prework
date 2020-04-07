561f3b9f79980cd0de33145b5d1ba86ad7e14f86
Tomasz Rosik <Bosik27@gmail.com>
2020-04-07 23:35:18 +0200

    Add funktion for random wariable and user input

--- js/script.js
+++ js/script.js
@@ -1,20 +1,54 @@
+function getMoveName(argMoveId){
+  if(argMoveId == 1){
+    console.log('komputer wylosował ' + argMoveId);
+    return 'kamień';
+  }else if (argMoveId == 2){
+    console.log('komputer wylosował ' + argMoveId);
+    return 'papier';
+  }else if (argMoveId == 3){
+    console.log('komputer wylosował ' + argMoveId);
+    return "nożyce";
+  }
+  printMessage('Nie znam ruchu o id ' + argMoveId + '.');
+  return 'nieznany ruch';
+}
+function displayResult(argComputerMove, argPlayerMove){
+  if ( argComputerMove == 'kamień' && argPlayerMove == 'papier'){
+    console.log(argComputerMove, argPlayerMove);
+    return 'TY wygrywasz!';
+  }else if( argComputerMove == 'kamień' && argPlayerMove == 'nożyce'){
+    console.log(argComputerMove, argPlayerMove);
+    return 'JA wygrywam!';
+  }else if( argComputerMove == 'kamień' && argPlayerMove == 'kamień'){
+    console.log(argComputerMove, argPlayerMove);
+    return 'MAMY remis!';
+  }
+  if ( argComputerMove == 'papier' && argPlayerMove == 'nożyce'){
+    console.log(argComputerMove, argPlayerMove);
+    return 'TY wygrywasz!';
+  }else if ( argComputerMove == 'papier' && argPlayerMove == 'kamień'){
+    return 'JA wygrywam!';
+  }else if( argComputerMove == 'papier' && argPlayerMove == 'papier'){
+    console.log(argComputerMove, argPlayerMove);
+    return 'MAMY remis!';
+  }
+  if( argComputerMove == 'nożyce' && argPlayerMove == 'kamień'){
+    return 'TY wygrywasz!';
+  }else if ( argComputerMove == 'nożyce' && argPlayerMove == 'papier'){
+    console.log(argComputerMove, argPlayerMove);
+    return 'JA wygrywam!';
+  }else if( argComputerMove == 'nożyce' && argPlayerMovee == 'nożyce'){
+    return 'MAMY remis!';
+  }
+}
+
 // losowanie zmiennej przez komputer
 
 let randomNumber = Math.floor(Math.random() * 3 + 1);
 
 console.log('Wylosowana liczba to: ' + randomNumber);
 
-if(randomNumber == 1){
-  computerMove = 'kamień';
-
-}else if(randomNumber == 2){
-  computerMove = 'papier';
-
-}else(randomNumber == 3){
-  computerMove = 'nożyce';
-}
-
-let computerMove = 'nieznany ruch';
+let computerMove = getMoveName(randomNumber);
 
 // podanie zmiennej przez urzytownika
 
@@ -22,45 +56,6 @@ let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.
 
 console.log('Gracz wpisał: ' + playerInput);
 
-let playerMove = 'nieznany ruch';
+let playerMove = getMoveName(playerInput);
 
-if(playerInput == '1'){
-  playerMove = 'kamień';
-
-}else if(playerInput == '2'){
-  playerMove = 'papier';
-
-}else(playerInput == '3'){
-  playerMove = 'nożyce';
-}
- // Podanie wyniku rundy
- 
-if( computerMove == 'kamień' && playerMove == 'papier'){
-  printMessage('Ja wybrałem ' + computerMove + '. Ty wybrałeś' + playerMove ' w takim razie TY wygrywasz!');
-}else if ( computerMove == 'kamień' && playerMove == 'nożyce'){
-  printMessage('Ja wybrałem ' + computerMove + '. Ty wybrałeś' + playerMove ' w takim razie JA wygrywam!');
-}else if( computerMove == 'kamień' && playerMove == 'kamień'){
-  printMessage('Ja wybrałem ' + computerMove + '. Ty wybrałeś' + playerMove ' w takim razie MAMY remis!');
-}else ( computerMove == 'kamień' && playerMove == 'nieznany ruch'){
-  printMessage('Ja wybrałem ' + computerMove + '. Ty wybrałeś' + playerMove ' to liczka spoza przedziału, jeżeli nie rozumiesz tak prostych zasad to lepiej daj sobie spokuj z tą grą!');
-}
-
-if( computerMove == 'papier' && playerMove == 'nożyce'){
-  printMessage('Ja wybrałem ' + computerMove + '. Ty wybrałeś' + playerMove ' w takim razie TY wygrywasz!');
-}else if ( computerMove == 'papier' && playerMove == 'kamień'){
-  printMessage('Ja wybrałem ' + computerMove + '. Ty wybrałeś' + playerMove ' w takim razie JA wygrywam!');
-}else if( computerMove == 'papier' && playerMove == 'papier'){
-  printMessage('Ja wybrałem ' + computerMove + '. Ty wybrałeś' + playerMove ' w takim razie MAMY remis!');
-}else ( computerMove == 'papier' && playerMove == 'nieznany ruch'){
-  printMessage('Ja wybrałem ' + computerMove + '. Ty wybrałeś' + playerMove ' to liczka spoza przedziału, jeżeli nie rozumiesz tak prostych zasad to lepiej daj sobie spokuj z tą grą!');
-}
-
-if( computerMove == 'nożyce' && playerMove == 'kamień'){
-  printMessage('Ja wybrałem ' + computerMove + '. Ty wybrałeś' + playerMove ' w takim razie TY wygrywasz!');
-}else if ( computerMove == 'nożyce' && playerMove == 'papier'){
-  printMessage('Ja wybrałem ' + computerMove + '. Ty wybrałeś' + playerMove ' w takim razie JA wygrywam!');
-}else if( computerMove == 'nożyce' && playerMove == 'nożyce'){
-  printMessage('Ja wybrałem ' + computerMove + '. Ty wybrałeś' + playerMove ' w takim razie MAMY remis!');
-}else ( computerMove == 'nożyce' && playerMove == 'nieznany ruch'){
-  printMessage('Ja wybrałem ' + computerMove + '. Ty wybrałeś' + playerMove ' to liczka spoza przedziału, jeżeli nie rozumiesz tak prostych zasad to lepiej daj sobie spokuj z tą grą!');
-}
+//displayResult(argComputerMove, argPlayerMove);
