b784bc4d05312ce3df2b616f3b37f8ee0b253574
Tomasz Rosik <Bosik27@gmail.com>
2020-04-08 21:49:31 +0200

    Fixed displayResult function

--- js/script.js
+++ js/script.js
@@ -12,6 +12,7 @@ function getMoveName(argMoveId){
   printMessage('Nie znam ruchu o id ' + argMoveId + '.');
   return 'nieznany ruch';
 }
+
 function displayResult(argComputerMove, argPlayerMove){
   if ( argComputerMove == 'kamień' && argPlayerMove == 'papier'){
     console.log(argComputerMove, argPlayerMove);
@@ -48,7 +49,8 @@ let randomNumber = Math.floor(Math.random() * 3 + 1);
 
 console.log('Wylosowana liczba to: ' + randomNumber);
 
-let computerMove = getMoveName(randomNumber);
+let argComputerMove = getMoveName(randomNumber);
+
 
 // podanie zmiennej przez urzytownika
 
@@ -56,6 +58,7 @@ let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.
 
 console.log('Gracz wpisał: ' + playerInput);
 
-let playerMove = getMoveName(playerInput);
+let argPlayerMove = getMoveName(playerInput);
 
-//displayResult(argComputerMove, argPlayerMove);
+
+printMessage (displayResult(argComputerMove,argPlayerMove));
